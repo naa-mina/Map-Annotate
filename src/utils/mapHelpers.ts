ac00fae4ac3314246eafb70fd4c9ba9d4d77ec11
naa-mina <yasbonney@gmail.com>
2025-07-18 17:46:50 +0200

    exports saved as a json

--- src/utils/mapHelpers.ts
+++ src/utils/mapHelpers.ts
@@ -105,7 +105,7 @@ export const exportLayerGeoJSON = (layer: LayerData) => {
   const url = URL.createObjectURL(blob);
   const a = document.createElement('a');
   a.href = url;
-  a.download = `${layer.name}.geojson`;
+  a.download = `${layer.name}.json`;
   a.click();
   URL.revokeObjectURL(url);
 };
